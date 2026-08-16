dba25050eb9ca70a1152c885b68f9289f5be8922
Rohit Kr. Singh <rohit4524l@gmail.com>
2026-08-16 10:40:34 +0530

    chore: add helmet security headers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
